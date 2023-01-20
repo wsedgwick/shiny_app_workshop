6e34dcf08214c8bfdea0229596b4462ecdced0ad
wsedgwick <wsedgwick0@gmail.com>
2023-01-20 09:53:16 -0800

    shiny app

---
+++ single-file-app/app.R
@@ -0,0 +1,13 @@
+# load packages ----
+library(shiny)
+
+# ui ----
+ui <- fluidPage()
+
+# server ----
+server <- function(input, output) {}
+
+# combine ui & server ----
+shinyApp(ui = ui, server = server)
+
+
